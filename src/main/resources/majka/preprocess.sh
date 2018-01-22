f67d0087f725ba295428146c99684f11b8a159f1
Lukáš Kurčík <lukas.kurcik@gmail.com>
2018-01-22 23:51:29 +0100

    Removed redundant line from preprocess.sh

--- src/main/resources/majka/preprocess.sh
+++ src/main/resources/majka/preprocess.sh
@@ -12,6 +12,4 @@ majkovane=$(for X in $words ; do
 done);
 
 #Remove stop words and saves them into file in second argument
-stopwords=$(cat stop_words.txt)
-
 echo " $majkovane " | sed -f stop_words.sed | tr '\n' ' ' | sed -e "s/  */ /g" > $2
