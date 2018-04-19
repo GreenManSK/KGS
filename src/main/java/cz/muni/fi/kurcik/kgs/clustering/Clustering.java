be0154c39cda15d731f156c6d397b9e8ab67331b
Lukáš Kurčík <lukas.kurcik@gmail.com>
2018-04-19 14:41:11 +0200

    Clustering todo

--- src/main/java/cz/muni/fi/kurcik/kgs/clustering/Clustering.java
+++ src/main/java/cz/muni/fi/kurcik/kgs/clustering/Clustering.java
@@ -12,10 +12,13 @@ public interface Clustering {
     String VOCAB_FILE = "vocab.txt";
     String CORPUS_FILE = "corpus.dat";
     String CLUSTERING_FILE = "clusters.txt";
+    String URL_CLUSTER_FILE = "url-cluster.txt";
 
     /**
      * Takes prerocessed data from CLUSTERING_FILES_DIR and makes clusters from them.
      * Saves list of topic probabilities for each document into file and each cluster into separate file
+     * Saves all url-cluster pairs into URL_CLUSTER_FILE
+     * @todo: Saves all url-cluster pairs into URL_CLUSTER_FILE
      * @throws IOException when there is problem with file IO
      */
     void cluster() throws IOException;
